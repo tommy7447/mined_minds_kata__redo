9d90e1d45f49615968e6dd0eddd2711314e8f194
Tommy Rohanna <Tommy Rohanna>
2017-01-19 09:16:03 -0500

    linkedtestfiletofunctionfile

---
+++ minedmindskata2/function.rb
@@ -0,0 +1,3 @@
+def mined_mind_function(number)
+		1
+end
--- minedmindskata2/testfunction.rb
+++ minedmindskata2/testfunction.rb
@@ -1,8 +1,6 @@
 require 'minitest/autorun'
+require_relative 'function.rb'
 
-	def mined_mind_function(number)
-		1
-	end
 class TestMMFunctions < Minitest::Test
 	def test_one_equals_one()
 		assert_equal(1,1)
