9d39bda13127d00e5aafef1a8c42aa701a567d6c
Tommy Rohanna <Tommy Rohanna>
2017-01-19 09:07:29 -0500

    testreturns1

--- minedmindskata2/testfunction.rb
+++ minedmindskata2/testfunction.rb
@@ -1,8 +1,16 @@
 require 'minitest/autorun'
 
-
+	def mined_mind_function(number)
+		1
+	end
 class TestMMFunctions < Minitest::Test
 	def test_one_equals_one()
 		assert_equal(1,1)
 	end
+	def test_mined_minds_function_returns1
+		assert_equal(1,mined_mind_function(1))
+		
+	end
+	
+
 end
